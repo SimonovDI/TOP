a146caef14104a7383c099cdd9e68b73403cca07
SimomovDI <denigsim@yandex.ru>
2022-12-08 15:18:06 +0300

    DZ ООП(self)

--- main.py
+++ main.py
@@ -93,6 +93,80 @@ import re
 #     t.write(text3)
     
 
+# ООП
+
+# class Book:
+#     name = 'name'
+#     year = 'year'
+#     publisher = 'publisher'
+#     genre = 'genre'
+#     autor = 'autor'
+#     price = 'price'
+#     
+#     def input_info(self,first_name,year,publisher,genre,autor,price):
+#         self.name = first_name
+#         self.year = year
+#         self.publisher = publisher
+#         self.genre = genre
+#         self.autor = autor
+#         self.price = price
+#     
+#     def print_info(self):
+#         print()
+#         print("="*40)
+#         print(f'Название книги: {self.name}\nГод выпуска: {self.year}\nИздатель: {self.publisher}\nЖанр: {self.genre}\nАвтор: {self.autor}\nЦена: {self.price}')
+#         print("="*40)
+#         
+#         
+#     def set_name(self,name):
+#         self.name = name
+#             
+#     def get_name(self):
+#         return self.name
+#         
+#         
+#     def set_year(self,year):
+#         self.yaer = year
+#         
+#     def get_year(self):
+#         return self.year
+#         
+#         
+#     def set_publisher(self,publisher):
+#         self.publisher=publisher
+#             
+#     def get_publisher(self):
+#         return self.publisher
+#         
+#         
+#     def set_ganre(self,genre):
+#         self.ganre = genre
+#             
+#     def get_genre(self):
+#         return self.genre
+#         
+#         
+#     def set_autor(self,autor):
+#         self.autor=autor
+#             
+#     def get_autor(self):
+#         return self.autor
+#         
+#         
+#     def set_price(self,price):
+#         self.price = price
+#             
+#     def get_price(self):
+#         return self.price
+# h1 = Book()
+# h1.input_info('Пираты','1822','Constable and Co.','History','Walter Scott','150$')
+# h1.print_info()
+# print(h1.get_name())
+# print(h1.get_autor())
+# h1.set_price('100$')
+# h1.print_info()            
+        
+
 
 
 
